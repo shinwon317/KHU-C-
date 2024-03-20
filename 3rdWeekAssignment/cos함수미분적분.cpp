56cb46bc9b9516dbc6437f5510b572fc72999780
haho0916 <159134004+haho0916@users.noreply.github.com>
2024-03-20 15:12:53 +0900

    Add files via upload

---
+++ 3rdWeekAssignment/cos함수미분적분.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void main() {
+    float a, b, t, dt, num, anal; //differentiation
+    cout << "a*cos(bt)" << endl;
+    cout << "what is a,b ?" << endl;
+    cin >> a >> b;
+    cout << "what is t, dt?" << endl;
+    cin >> t >> dt;
+    anal = -1 * a * b * sin(b * t);
+    num = a * (cos(b * (t + dt)) - cos(b * t)) / dt;
+    cout << "num " << num << endl;
+    cout << "anal " << anal << endl;
+    cout << "error= " << abs((num - anal)) / anal * 100 << "%" << endl;
+
+    //integration
+    float c, d;
+    int N;
+    cout << "c, d, N ?" << endl;
+    cin >> c >> d >> N;
+    anal = a * (sin(b * d) - sin(b * c));
+    dt = (d - c) / N;
+    num = 0;
+    for (int i = 0; i < N; i++) {
+        num += a * cos(b * (c + dt * i)) * dt;
+    }
+    cout << "num " << num << endl;
+    cout << "anal " << anal << endl;
+    cout << "error= " << abs((num - anal)) / anal * 100 << "%" << endl;
+
+}
